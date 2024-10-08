e9be25693855ed6c19ea9172b7b36abdabe9e468
nour7anelsto7y@gmail.com <nour7anelsto7y@gmail.com>
2024-10-08 21:29:20 +0300

    Add AuthTests for register and login apis.

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+
+    public function test_user_can_register()
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['message', 'token']);
+    }
+
+    public function test_user_can_login()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt($password = 'password')
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['token']);
+    }
+}
